50d1b7c34aab52ec6755a5e6d43cb1745fae3f54
Marcelo <mar.rose1999@gmail.com>
2020-04-20 15:55:44 -0300

    Alterado layout e mudanças no código da ação do botão compilar

--- src/notfalsecompiler/compiler/Semantico.java
+++
@@ -1,9 +0,0 @@
-package notfalsecompiler.compiler;
-
-public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Acao #"+action+", Token: "+token);
-    }	
-}
